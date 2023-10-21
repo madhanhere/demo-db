c35f1c5fa3214ce19a20a05d79293b594ddf22c6
email <madhankumar@calibraint.com>
2023-10-22 03:50:54 +0530

    added server support and .gitignore

---
+++ sever.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const cors = require('cors');
+const router = jsonServer.router("db.json");
+server.use(cors);
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 8000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
